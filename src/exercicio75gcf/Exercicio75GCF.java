4f00bf4a3c019ecf9fdb13fa44ab5355f3a8d889
AndersonHilarioJr <anndyhj@gmail.com>
2018-06-19 11:32:06 -0300

    Feito a função de calcular Fatorial e inserido um menu para dividir as operações

--- src/exercicio75gcf/Exercicio75GCF.java
+++ src/exercicio75gcf/Exercicio75GCF.java
@@ -7,14 +7,52 @@ import java.util.*;
  */
 public class Exercicio75GCF {
 
+    static Scanner s = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Informe um número para ser calculado a tabuada:");
-        int valor = s.nextInt();
-        
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(valor + " x " + i + " = " + valor*i);
+
+        int opcao = -1;
+        while (opcao != 0) {
+            System.out.println("Informe uma opção:");
+            System.out.println("1 - Calcular a tabuada");
+            System.out.println("2 - Calcular o fatorial");
+            System.out.println("0 - Sair");
+            opcao = s.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    calculaTabuada();
+                    break;
+                case 2:
+                    calculaFatorial();
+                    break;
+                case 0:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Informe um valor válido");
+                    break;
+            }
         }
     }
-    
+
+    private static void calculaTabuada() {
+        System.out.println("Informe um número para ser calculado a tabuada:");
+        int valor = s.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(valor + " x " + i + " = " + valor * i);
+        }
+    }
+
+    private static void calculaFatorial() {
+        int fat = 1;
+        System.out.println("Informe um número para ser calculado o fatorial:");
+        int valor = s.nextInt();
+        for (int i = 2; i <= valor; i++) {
+            fat *= i;
+        }
+        System.out.println("O fatorial de " + valor + " é igual a " + fat);
+    }
+
 }
