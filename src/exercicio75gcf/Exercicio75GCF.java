de93e73aeb269ec3f49b9cb7685ab67209533920
AndersonHilarioJr <anndyhj@gmail.com>
2018-06-19 13:14:13 -0300

    Adicionada funcao de bascara

--- src/exercicio75gcf/Exercicio75GCF.java
+++ src/exercicio75gcf/Exercicio75GCF.java
@@ -16,6 +16,7 @@ public class Exercicio75GCF {
             System.out.println("Informe uma opção:");
             System.out.println("1 - Calcular a tabuada");
             System.out.println("2 - Calcular o fatorial");
+            System.out.println("3 - Calcular bascara");
             System.out.println("0 - Sair");
             opcao = s.nextInt();
 
@@ -25,6 +26,9 @@ public class Exercicio75GCF {
                     break;
                 case 2:
                     calculaFatorial();
+                    break;
+                case 3:
+                    calculaBascara();
                     break;
                 case 0:
                     System.exit(0);
@@ -55,4 +59,17 @@ public class Exercicio75GCF {
         System.out.println("O fatorial de " + valor + " é igual a " + fat);
     }
 
+    private static void calculaBascara() {
+        System.out.println("Informe o valor 'a' da equeção");
+        double a = s.nextInt();
+        System.out.println("Informe o valor 'b' da equeção");
+        double b = s.nextInt();
+        System.out.println("Informe o valor 'c' da equeção");
+        double c = s.nextInt();
+        double delta = Math.sqrt(Math.pow(b, 2) - 4*a*c);
+        double x1 = (-b + delta)/(2*a);
+        double x2 = (-b - delta)/(2*a);
+        System.out.println("x1 = " + x1);
+        System.out.println("x2 = " + x2);
+    }
 }
